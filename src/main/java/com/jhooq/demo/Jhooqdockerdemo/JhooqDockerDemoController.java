7496e41d78073ff7cb5059899ec8cb7a61a59e06
vogets <76695052+vogets@users.noreply.github.com>
2021-05-30 15:23:07 +0530

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -2,12 +2,20 @@ package com.jhooq.demo.Jhooqdockerdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
 
 @RestController
 public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Docker Demo - Hello Jhooq";
+        String ip="";
+         try {
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            System.out.printf("%s", ip);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Docker Demo - Hello Jhooq from: "+ip;
     }
 }
